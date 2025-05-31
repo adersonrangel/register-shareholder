24aa156649a26c166063d01fb9e49a07c809e4fb
Aderson Rangel <adersonrangel@gmail.com>
2025-05-31 12:38:53 -0500

    Add total employees counter

--- Pages/Index.cshtml.cs
+++ Pages/Index.cshtml.cs
@@ -7,13 +7,16 @@ public class IndexModel : PageModel
 {
     private readonly ILogger<IndexModel> _logger;
 
+    public List<string> Empleados { get; set; }
+
     public IndexModel(ILogger<IndexModel> logger)
     {
         _logger = logger;
+        Empleados = new List<string>();
     }
 
     public void OnGet()
     {
-
+        
     }
 }
--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,7 @@
 import { Empleado } from "../uc/empleados/Empleado";
 
+let contadorEmpleados = window.initialEmployeeCount || 0;
+
 const btnCrear = document.getElementById("btnAddUser");
 
 btnCrear?.addEventListener("click", () => {
@@ -7,17 +9,26 @@ btnCrear?.addEventListener("click", () => {
   const txtApellido = document.getElementById("txtApellido").value;
   const txtEdad = document.getElementById("txtEdad").value;
 
+  const noDataDiv = document.getElementById("noData");
+  if (noDataDiv) {
+    noDataDiv.style.display = "none";
+  }
   agregarEmpleado(txtNombre, txtApellido, txtEdad);
 });
 
 function agregarEmpleado(nombre, apellido, edad) {
   let empleado1 = new Empleado(nombre, apellido, edad);
-  console.log(empleado1.nombre); // Juan
 
   let listaEmpleados = document.getElementById("empleados");
   if (listaEmpleados) {
     let empleadoItem = document.createElement("li");
     empleadoItem.textContent = `${empleado1.nombre} ${empleado1.apellido}, Edad: ${empleado1.edad}`;
     listaEmpleados.appendChild(empleadoItem);
+    contadorEmpleados++;
+
+    const contadorElement = document.getElementById("totalEmpleados");
+    if (contadorElement) {
+      contadorElement.textContent = `Total de empleados: ${contadorEmpleados}`;
+    }
   }
 }
